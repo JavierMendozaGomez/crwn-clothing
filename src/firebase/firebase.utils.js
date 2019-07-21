4cb1327ab766aaf7d279a5f7f77587774cc66571
javier <javierenriquemendozagomez@gmail.com>
2019-07-21 18:32:10 +0100

    added signinsignout status

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,28 @@ const config = {
     messagingSenderId: "353089079523",
     appId: "1:353089079523:web:83c3c316324dcca8"
 };
+export const createUserProfileDocument =  async (userAuth, additionalData) => {
+    if (!userAuth) {
+        return;
+    } 
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if(!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+};
 
 firebase.initializeApp(config);
 
