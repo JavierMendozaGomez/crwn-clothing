bf5213f6df61a12c8e491ceb0f1430edf9e76481
javier <javierenriquemendozagomez@gmail.com>
2019-07-22 21:03:20 +0100

    Created Signin

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import {auth, createUserProfileDocument} from '../../firebase/firebase.utils';
+
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            displayName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        const {displayName, email, password, confirmPassword} = this.state;
+        if (password !== confirmPassword) {
+            alert('DIFFERENT PASSWORDS!')
+            return;
+        }
+        try {
+            const {user} = await auth.createUserWithEmailAndPassword(email, password);
+            await createUserProfileDocument(user, {displayName});
+            this.setState({
+                displayName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    handleChange = event => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        const {
+            displayName,
+            email, 
+            password,
+            confirmPassword,
+        } = this.state;
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <FormInput 
+                    type='text'
+                    name='displayName'
+                    label='Display Name'
+                    value={displayName}
+                    handleChange={this.handleChange}
+                    required
+                />
+                <FormInput
+                    type='email'
+                    name='email'
+                    label='Email'
+                    value={email}
+                    handleChange={this.handleChange}
+                    required
+                />
+                <FormInput 
+                    type='password'
+                    name='password'
+                    label='Password'
+                    value={password}
+                    handleChange={this.handleChange}
+                    required
+                />
+                <FormInput 
+                    type='password'
+                    name='confirmPassword'
+                    label='Confirm password'
+                    value={confirmPassword}
+                    handleChange={this.handleChange}
+                    required
+                />
+                <CustomButton type='submit'>SIGN UP</CustomButton>            
+            </form>
+        )
+    }
+}
+
+export default SignUp;
