ef6bcc7addff840679024f4893a1a38d67fde58d
Javier <javierenriquemendozagomez@gmail.com>
2019-08-11 11:32:03 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import ShopPage from './pages/shop/shop.component';
 import SignInSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {connect} from 'react-redux';
+import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component {
   constructor(){
@@ -49,7 +51,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
@@ -60,4 +62,9 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch()
+})
+
+
+export default connect(null, )(App);
