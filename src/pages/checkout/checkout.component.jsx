11e8d3a10de93dd7954048003046830b5ccb27d7
javier <javierenriquemendozagomez@gmail.com>
2019-08-17 15:46:58 +0100

    added selector of checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+import {
+    selectCartItems,
+    selectCartTotal
+} from '../../redux/cart/car.selectors'
 import './checkout.styles.scss';
 
-const CheckoutPage = () => (
-    <div>
-        CHECKOUT PAGE
+const CheckoutPage = ({cartItems, cartTotal}) => (
+    <div className='checkout-page'>
+        <div className='checkout-header'>
+            <div className='header-block'>
+                <span>Product</span>
+            </div>
+            <div className='header-block'>
+                <span>Descripition</span>
+            </div>
+            <div className='header-block'>
+                <span></span>
+            </div>
+            <div className='header-block'>
+                <span>Quantity</span>
+            </div>
+            <div className='header-block'>
+                <span>Price</span>
+            </div>
+            <div className='header-block'>
+                <span>Remove</span>
+            </div>
+        </div>
+        {
+            cartItems.map(cartItem => 
+                cartItem.name
+            )
+        }
+        <div classname='total'>
+            <span>Total: ${cartTotal}</span>
+        </div>
+        
     </div>
 );
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems,
+    cartTotal: selectCartTotal
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
--- src/redux/cart/car.selectors.js
+++ src/redux/cart/car.selectors.js
@@ -17,3 +17,9 @@ export const selectCartHidden = createSelector(
     [selectCart],
     cart => cart.hidden
 );
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => 
+        cartItems.reduce((total, item) => (total + (item.quantity * item.price)), 0)
+);
